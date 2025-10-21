4961205d6bdcb98f84df6f2fa055c0835c15a757
Awalwyn <118495079+Awalwyn@users.noreply.github.com>
2025-10-21 00:46:05 -0500

    Added user auth features: user registration, login and profile endpoints

---
+++ src/main/java/com/outside/api/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.outside.api.repository;
+import com.outside.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
